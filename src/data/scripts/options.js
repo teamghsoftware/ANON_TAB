54a850b768b33f13ca8f6bb5270910a5ca7b02da
0xsobky <0xsobky@gmail.com>
2016-12-23 12:12:47 +0200

    Improve the options reset functionality

--- src/data/scripts/options.js
+++ src/data/scripts/options.js
@@ -1,35 +1,57 @@
+var proxy = 'https://feedback.googleusercontent.com/gadgets/proxy?container=fbk&url=';
+var proxyInpt = document.getElementById('proxyInpt');
+
 /**
- * Save options to `chrome.storage`.
- * @param ev {object}, a submit event.
+ * Display options status.
+ * @param message {string}, a message to display.
  * return void.
  */
-function saveOptions(ev) {
-    var proxy = document.getElementById('proxyInpt').value;
+function updateStatus(message) {
+    // Update status to let the user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = message;
+    setTimeout(function() {
+        status.textContent = '';
+    }, 750);
+}
+
+/**
+ * Save options to `chrome.storage`.
+ * return void.
+ */
+function saveOptions() {
+    var proxy = proxyInpt.value;
     chrome.storage.local.set({
         proxy: proxy
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Updates saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
     });
-    ev.preventDefault();
 }
 
 /**
  * Restore options from `chrome.storage`.
+ * @param reset {boolean} optional, restores default options.
  * return void.
  */
-function restoreOptions() {
-    chrome.storage.local.get({
-        proxy: 'https://feedback.googleusercontent.com/gadgets/proxy?container=fbk&url='
-    }, function(items) {
-        document.getElementById('proxyInpt').value = items.proxy;
-    });
+function restoreOptions(reset) {
+    if (reset === true) {
+        proxyInpt.value = proxy;
+    } else {
+        chrome.storage.local.get({
+            proxy: proxy
+        }, function(items) {
+            proxyInpt.value = items.proxy;
+        });
+    }
 }
 
+document.getElementById('form').addEventListener('submit', function(ev) {
+        saveOptions();
+        updateStatus('Options saved.');
+        ev.preventDefault();
+});
+document.getElementById('reset').addEventListener('click', function(ev) {
+        restoreOptions(true);
+        saveOptions();
+        updateStatus('Options reset.');
+        ev.preventDefault();
+});
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('form').addEventListener('submit',
-    saveOptions);
