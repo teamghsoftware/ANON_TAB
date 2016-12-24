76b733a97f168d6568955ec4cc61e1d70629cf3c
0xsobky <0xsobky@gmail.com>
2016-12-24 03:14:05 +0200

    Revert "Improve the configuration page"
    This reverts commit b46aee8bd0ed95fa3072b4a2228ab15c43ddcda1.

--- src/data/scripts/options.js
+++ src/data/scripts/options.js
@@ -4,33 +4,20 @@ var proxyInpt = document.getElementById('proxyInpt');
 /**
  * Display options status.
  * @param message {string}, a message to display.
- * @param isPersistent {boolean}, displays a message persistently.
- * @return void.
+ * return void.
  */
-function updateStatus(message, isPersistent) {
-    var interval;
+function updateStatus(message) {
+    // Update status to let the user know options were saved.
     var status = document.getElementById('status');
     status.textContent = message;
-    if (isPersistent) {
-        interval = setInterval(function() {
-            if (/\.{3}$/.test(status.textContent)) {
-                status.textContent = status.textContent.slice(0, -2);
-            } else if (message.indexOf(status.textContent) === 0) {
-                status.textContent += '.';
-            } else {
-                clearInterval(interval);
-            }
-        }, 300);
-    } else {
-        setTimeout(function() {
-            status.textContent = '';
-        }, 2000);
-    }
+    setTimeout(function() {
+        status.textContent = '';
+    }, 750);
 }
 
 /**
  * Save options to `chrome.storage`.
- * @return void.
+ * return void.
  */
 function saveOptions() {
     var proxy = proxyInpt.value;
@@ -42,7 +29,7 @@ function saveOptions() {
 /**
  * Restore options from `chrome.storage`.
  * @param reset {boolean} optional, restores default options.
- * @return void.
+ * return void.
  */
 function restoreOptions(reset) {
     if (reset === true) {
@@ -56,32 +43,9 @@ function restoreOptions(reset) {
     }
 }
 
-/**
- * Change the current proxy server to a new one.
- * @return void.
- */
-function updateProxy() {
-    var isValid = false;
-    var proxy = proxyInpt.value;
-    var xhrReq = new XMLHttpRequest();
-    updateStatus('Validating proxy...', true);
-    xhrReq.onload = function() {
-        if (this.status === 200) {
-            saveOptions();
-            updateStatus('Changes saved.');
-            proxyInpt.style.boxShadow = '0 0 10px green inset';
-        }
-    };
-    xhrReq.onerror = function() {
-        updateStatus("Couldn't validate proxy server.");
-        proxyInpt.style.boxShadow = '0 0 10px red inset';
-    }
-    xhrReq.open('GET', proxy + 'http://example.com');
-    xhrReq.send();
-}
-
 document.getElementById('form').addEventListener('submit', function(ev) {
-        updateProxy();
+        saveOptions();
+        updateStatus('Options saved.');
         ev.preventDefault();
 });
 document.getElementById('reset').addEventListener('click', function(ev) {
